5be028c3280dacc4a0f9632f5461105c69df5b57
jo-vana <jovananukic85@gmail.com>
2023-09-01 11:24:58 +0200

    Popup fixes

--- main.js
+++ main.js
@@ -7,10 +7,10 @@ function showPopup(){
         jQuery('#popup_template').removeClass('show');
         jQuery('body').removeClass('overlay');
     })
-    if(window.location.pathname == "/"){    //set pathname to Home page
+    if(window.location.pathname == "/index.html"){    //set pathname to Home page
         setTimeout(() => {
             jQuery('#popup_template').addClass('show');
-            jQuery('body').addClass('overlay');
+            jQuery('body').addClass('overlay-popup');
             jQuery('#get_email').on('click', function(){
                 let inputValue = jQuery('#newsletter_input').val(); //variable with email value
                 let inputValueValid = document.getElementById("newsletter_input").validity.valid;
@@ -19,7 +19,7 @@ function showPopup(){
                 }
                 if(inputValue && inputValueValid) {
                     jQuery('#popup_template').removeClass('show');
-                    jQuery('body').removeClass('overlay');
+                    jQuery('body').removeClass('overlay-popup');
                 }
             })
         }, 5000);
